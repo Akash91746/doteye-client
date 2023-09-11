0d5587329c0a93322c61421d8f2bee47c1b2f2a9
AKash <sahaniakash91@gmail.com>
2023-09-11 18:10:01 +0530

    added page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import HomeScreen from '../src/HomeScreen';
 import { NextPage } from 'next';
+import Head from 'next/head';
 
 const HomePage: NextPage = () => {
-    return <HomeScreen />
+    return <React.Fragment>
+        <Head>
+            <title>dotEye</title>
+        </Head>
+        <HomeScreen />
+    </React.Fragment>
+
+
 }
 
 export default HomePage;
